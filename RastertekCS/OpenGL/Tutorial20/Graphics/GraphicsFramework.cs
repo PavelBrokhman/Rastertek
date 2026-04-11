feed1703f6ec41bcfde93a56041dbc0f095b8be0
Pavel Brokhman <brokhmanpavel@live.ca>
2026-04-11 16:52:08 -0400

    Test: OpenGL Tut20 use LH lookAt + LH projection + Cw front face (matches Windows DX rendering)

--- RastertekCS/OpenGL/Tutorial20/Graphics/GraphicsFramework.cs
+++ RastertekCS/OpenGL/Tutorial20/Graphics/GraphicsFramework.cs
@@ -52,10 +52,10 @@ public class GraphicsFramework
     public bool Frame()
     {
         // Update rotation each frame.
-        m_rotation += 0.0174532925f * 1.0f;
-        if (m_rotation > MathF.Tau)
+        m_rotation -= 0.0174532925f * 1.0f;
+        if (m_rotation < 0.0f)
         {
-            m_rotation -= MathF.Tau;
+            m_rotation += MathF.Tau;
         }
 
         return Render(m_rotation);
