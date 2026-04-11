cd27ad372d510b3c6acefce3ea57f34432db26a5
Pavel Brokhman <brokhmanpavel@live.ca>
2026-04-11 07:23:07 -0400

    Add Windows Tut17 multi-texturing

---
+++ RastertekCS/Windows/Tutorial17/Graphics/GraphicsFramework.cs
@@ -0,0 +1,59 @@
+using Silk.NET.Maths;
+
+namespace RastertekCS.Windows.Tutorial17.Graphics;
+
+public class GraphicsFramework
+{
+    private DX11 m_DirectX;
+    private Camera m_Camera;
+    private Model m_Model;
+    private MultiTextureShader m_MultiTextureShader;
+
+    public bool Initialize(DX11 DirectX)
+    {
+        m_DirectX = DirectX;
+
+        m_Camera = new Camera();
+        m_Camera.SetPosition(0.0f, 0.0f, -5.0f);
+        m_Camera.Render();
+
+        m_Model = new Model();
+        if (!m_Model.Initialize(DirectX, "Models/square.txt", "Data/stone01.tga", "Data/dirt01.tga", true))
+            return false;
+
+        m_MultiTextureShader = new MultiTextureShader();
+        if (!m_MultiTextureShader.Initialize(DirectX)) return false;
+
+        return true;
+    }
+
+    public void Shutdown()
+    {
+        m_MultiTextureShader?.Shutdown();
+        m_Model?.Shutdown();
+        m_MultiTextureShader = null;
+        m_Model = null;
+        m_Camera = null;
+        m_DirectX = null;
+    }
+
+    public bool Frame() => Render();
+
+    private bool Render()
+    {
+        m_DirectX.BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
+
+        var world = m_DirectX.GetWorldMatrix();
+        var view = m_Camera.GetViewMatrix();
+        var projection = m_DirectX.GetProjectionMatrix();
+
+        m_Model.Render(m_DirectX);
+        m_Model.SetTextures(m_DirectX);
+
+        if (!m_MultiTextureShader.Render(m_DirectX, m_Model.GetIndexCount(), world, view, projection))
+            return false;
+
+        m_DirectX.EndScene();
+        return true;
+    }
+}
